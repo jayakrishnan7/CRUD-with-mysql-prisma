b2f9e71cd9fdbe276ad35a464cfcf7f1351a084e
jayakrishnan-mk <jayakrishnansfc43@gmail.com>
2022-11-01 16:51:58 +0530

    create and read comp

--- src/server/controllers/users.ts
+++ src/server/controllers/users.ts
@@ -29,129 +29,139 @@ const excelJS = require("exceljs");
 // import * as excelJS from 'exceljs'
 // import excel from 'exceljs';
 
-// var pool = mysql.createConnection({
-var pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "crud",
-});
+import { PrismaClient } from "@prisma/client";
+const { user } = new PrismaClient();
 
-// ....... all users fetching....................
+// ....... all users fetching................................................................
 const allUsers = async (req: Request, res: Response) => {
-  let skip = 0;
-  let limit = 10;
+  // let skip = 0;
+  // let limit = 10;
 
-  pool.query("SELECT * FROM students", (err, result, fields) => {
-    if (err) {
-      console.log(err);
-      res.send({ status: 0, data: err });
-    } else {
-      console.log(result);
-      res.send({ status: 0, data: result });
-    }
+  const users = await user.findMany({
+    select: {
+      username: true,
+      // posts: true
+    },
   });
+
+  // console.log('ussssssssssss', users);
+
+  res.json(users);
 };
 
 // .......  user  sign up   ....................
 const createPerson = async (req: Request, res: Response) => {
   try {
 
-    console.log('rrrrrrrrrrr', req.body);
-    
-    const { name, classNumber, email, password, phone, dob, photo, isDeleted } =
-      req.body;
+    const {username} = req.body;
 
-    const pswd = req.body.password;
-
-    // console.log('greattttttttt......', req.body);
-
-    // ...........................................password.encrypting............................................
-    var ciphertext = CryptoJS.AES.encrypt(pswd, "crud secret 763").toString();
-
-    console.log("encrypted text ....... cipherrrrrr", ciphertext);
-
-    const dateIn = req.body.dob;
-
-    const newDate = moment.utc(dateIn, "DD/MM/YYYY").toDate();
-    moment().format();
-
-    // console.log('newwwwwwwwww', newDate);
-
-    const userData = {
-      name: req.body.name,
-      classNumber: req.body.classNumber,
-      email: req.body.email,
-      password: ciphertext,
-      phone: req.body.phone,
-      dob: newDate,
-      photo: req.body.photo,
-      isDeleted: req.body.isDeleted,
-    };
-
-    // console.log('boddddd...............', name);
-
-    const checkUsername = `Select name FROM students WHERE name = ${req.body.name}`;
-    // execQuery(checkUsername, [])
-
-    // console.log('res..........',execQuery.toString());
-
-    pool.query(checkUsername, [name], (err, result, fields) => {
-      // console.log('rrrrrrrrrrrrrrrrr', result?.length);
-
-      if (!result?.length) {
-        // console.log("its a chance..........");
-
-        const sql = `Insert Into students (
-          name, 
-          classNumber, 
-          email, 
-          password, 
-          phone, 
-          dob,  
-          photo, 
-          isDeleted
-          ) VALUES ( ?, ?, ?,  ?, ?, ?, ?,  ? )`;
-        pool.query(
-          sql,
-          [
-            userData.name,
-            userData.classNumber,
-            userData.email,
-            userData.password,
-            userData.phone,
-            userData.dob,
-            userData.photo,
-            userData.isDeleted,
-          ],
-          (err, result, fields) => {
-            if (err) {
-              res.send({ status: 0, data: err });
-            } else {
-              // result.forEach((element: any) => {
-              //   if(element.constructor == Array) {
-              //     res.send('Inserted student id:  ' + element[0].id)
-              //   }
-              // })
-              res.send({ status: 1, data: result });
-            }
-          }
-        );
+    const userExists = await user.findUnique({
+      where : {
+        username
+      }, 
+      select: {
+        username: true
       }
-
-      // ...........................................password.decrypting.....................................................
-
-      const decryptUser = userData.password;
-
-      // console.log('deccccccrypt userrr', decryptUser);
-
-      var bytes = CryptoJS.AES.decrypt(decryptUser, "crud secret 763");
-      var originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-      console.log("decrypted password..........", originalText);
-
-      // ------------------------------------------------------------------------------------------------------------------
     });
+
+    if(userExists) {
+
+      res.status(400).json({message: "user already exists" })
+    }
+
+    const newUser = await user.create({
+      data: {
+        username
+      }
+    })
+
+    console.log('NewUser registered', newUser);
+    
+    res.json(newUser)
+
+
+
+
+
+
+
+
+
+
+    // console.log("rrrrrrrrrrr", req.body);
+    // const { name, classNumber, email, password, phone, dob, photo, isDeleted } =
+    //   req.body;
+    // const pswd = req.body.password;
+    // // console.log('greattttttttt......', req.body);
+    // // ...........................................password.encrypting............................................
+    // var ciphertext = CryptoJS.AES.encrypt(pswd, "crud secret 763").toString();
+    // console.log("encrypted text ....... cipherrrrrr", ciphertext);
+    // const dateIn = req.body.dob;
+    // const newDate = moment.utc(dateIn, "DD/MM/YYYY").toDate();
+    // moment().format();
+    // // console.log('newwwwwwwwww', newDate);
+    // const userData = {
+    //   name: req.body.name,
+    //   classNumber: req.body.classNumber,
+    //   email: req.body.email,
+    //   password: ciphertext,
+    //   phone: req.body.phone,
+    //   dob: newDate,
+    //   photo: req.body.photo,
+    //   isDeleted: req.body.isDeleted,
+    // };
+    // // console.log('boddddd...............', name);
+    // const checkUsername = `Select name FROM students WHERE name = ${req.body.name}`;
+    // // execQuery(checkUsername, [])
+    // // console.log('res..........',execQuery.toString());
+    // // pool.query(checkUsername, [name], (err, result, fields) => {
+    // //   // console.log('rrrrrrrrrrrrrrrrr', result?.length);
+    // //   if (!result?.length) {
+    // //     // console.log("its a chance..........");
+    // //     const sql = `Insert Into students (
+    // //       name,
+    // //       classNumber,
+    // //       email,
+    // //       password,
+    // //       phone,
+    // //       dob,
+    // //       photo,
+    // //       isDeleted
+    // //       ) VALUES ( ?, ?, ?,  ?, ?, ?, ?,  ? )`;
+    // //     pool.query(
+    // //       sql,
+    // //       [
+    // //         userData.name,
+    // //         userData.classNumber,
+    // //         userData.email,
+    // //         userData.password,
+    // //         userData.phone,
+    // //         userData.dob,
+    // //         userData.photo,
+    // //         userData.isDeleted,
+    // //       ],
+    // //       (err, result, fields) => {
+    // //         if (err) {
+    // //           res.send({ status: 0, data: err });
+    // //         } else {
+    // //           // result.forEach((element: any) => {
+    // //           //   if(element.constructor == Array) {
+    // //           //     res.send('Inserted student id:  ' + element[0].id)
+    // //           //   }
+    // //           // })
+    // //           res.send({ status: 1, data: result });
+    // //         }
+    // //       }
+    // //     );
+    // //   }
+    // //   // ...........................................password.decrypting.....................................................
+    // //   const decryptUser = userData.password;
+    // //   // console.log('deccccccrypt userrr', decryptUser);
+    // //   var bytes = CryptoJS.AES.decrypt(decryptUser, "crud secret 763");
+    // //   var originalText = bytes.toString(CryptoJS.enc.Utf8);
+    // //   console.log("decrypted password..........", originalText);
+    // //   // ------------------------------------------------------------------------------------------------------------------
+    // // });
   } catch (error) {
     console.log("errrrr", error);
     res.send({ status: 0, error: error });
@@ -186,19 +196,17 @@ const updateUser = async (req: Request, res: Response) => {
     //   isDeleted: req.body.isDeleted,
     // };
 
-
     // console.log('dddddddd', dob);
     // console.log('tttttt', typeof(dob));
-    
+
     // const dateFormatting = moment.utc(dob, "DD/MM/YYYY").toDate();
     // const newDate = moment.utc(dateFormatting).format('DD/MM/YYYY');
-    
-    // var formatted = moment(dob).format("DD-MM-YYYY");  
-    var formatted = moment(dob). format('YYYY-MM-DD')
 
-    console.log('newddddddd', formatted);
-    console.log('tyyyyyyy', typeof(formatted));
-    
+    // var formatted = moment(dob).format("DD-MM-YYYY");
+    var formatted = moment(dob).format("YYYY-MM-DD");
+
+    console.log("newddddddd", formatted);
+    console.log("tyyyyyyy", typeof formatted);
 
     const sql = `
     UPDATE students 
@@ -214,29 +222,26 @@ const updateUser = async (req: Request, res: Response) => {
         WHERE id = "${id}"
        `;
 
-
-    pool.query(
-      sql,
-      [name, classNumber, email, phone, dob, photo, isDeleted],
-      (err, result, fields) => {
-        if (err) {
-          res.send({ status: 0, data: err });
-        } else {
-          // result.forEach((element: any) => {
-          //   if(element.constructor == Array) {
-          //     res.send('Inserted student id:  ' + element[0].id)
-          //   }
-          // })
-          res.json({
-            message: "Updated successfully...",
-            status: 1,
-            data: result,
-          });
-        }
-      }
-    );
-
-
+    // pool.query(
+    //   sql,
+    //   [name, classNumber, email, phone, dob, photo, isDeleted],
+    //   (err, result, fields) => {
+    //     if (err) {
+    //       res.send({ status: 0, data: err });
+    //     } else {
+    //       // result.forEach((element: any) => {
+    //       //   if(element.constructor == Array) {
+    //       //     res.send('Inserted student id:  ' + element[0].id)
+    //       //   }
+    //       // })
+    //       res.json({
+    //         message: "Updated successfully...",
+    //         status: 1,
+    //         data: result,
+    //       });
+    //     }
+    //   }
+    // );
   } catch (error) {
     console.log(error);
     res.status(500).send("Updation failed!!");
@@ -254,17 +259,17 @@ const deletePerson = async (req: Request, res: Response) => {
       DELETE FROM students
       WHERE id = "${id}"`;
 
-    pool.query(sql, [id], (err, result, fields) => {
-      if (err) {
-        res.send({ status: 0, data: err });
-      } else {
-        res.json({
-          message: "Deleted successfully...",
-          status: 1,
-          data: result,
-        });
-      }
-    });
+    // pool.query(sql, [id], (err, result, fields) => {
+    //   if (err) {
+    //     res.send({ status: 0, data: err });
+    //   } else {
+    //     res.json({
+    //       message: "Deleted successfully...",
+    //       status: 1,
+    //       data: result,
+    //     });
+    //   }
+    // });
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
@@ -293,19 +298,19 @@ const searchUsers = async (req: Request, res: Response) => {
       searchText +
       "%'";
 
-    pool.query(sql, function (error, result) {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        if (!result.length) {
-          res.json({ message: "No students with this details!!!", result });
-        } else {
-          // console.log('search result......', result);
-          res.json({ message: "search result of students", result });
-        }
-      }
-    });
+    // pool.query(sql, function (error, result) {
+    //   if (error) {
+    //     console.log(error);
+    //     res.send(error);
+    //   } else {
+    //     if (!result.length) {
+    //       res.json({ message: "No students with this details!!!", result });
+    //     } else {
+    //       // console.log('search result......', result);
+    //       res.json({ message: "search result of students", result });
+    //     }
+    //   }
+    // });
   } catch (error) {
     console.log("errorrrrrrr", error);
     res.status(500).send(error);
@@ -344,86 +349,86 @@ const exportUsers = async (req: Request, res: Response) => {
 
   const sql = `SELECT * FROM students WHERE dob >= '${fromDate}' AND dob <= '${lastDate}'`;
 
-  pool.query(sql, function (error, result) {
-    if (error) {
-      console.log(error);
-      // res.send(error)
-    } else {
-      // console.log("rrrrrrrrrrrrrr", result);
+  //   pool.query(sql, function (error, result) {
+  //     if (error) {
+  //       console.log(error);
+  //       // res.send(error)
+  //     } else {
+  //       // console.log("rrrrrrrrrrrrrr", result);
 
-      if (Array.isArray(result)) {
-        result.forEach((student) => {
-          student.classNumber = counter;
-          worksheet.addRow(student);
-          counter++;
-        });
-      }
+  //       if (Array.isArray(result)) {
+  //         result.forEach((student) => {
+  //           student.classNumber = counter;
+  //           worksheet.addRow(student);
+  //           counter++;
+  //         });
+  //       }
 
-      worksheet.getRow(1).eachCell((cell: any) => {
-        cell.font = { bold: true };
-      });
-    }
+  //       worksheet.getRow(1).eachCell((cell: any) => {
+  //         cell.font = { bold: true };
+  //       });
+  //     }
 
-    try {
-      // console.log('tryyyyyyyyyyyyy reached');
+  //     try {
+  //       // console.log('tryyyyyyyyyyyyy reached');
 
-      async function excelFunction() {
-        const data = await workbook.xlsx.writeFile(v4() + ".xlsx");
+  //       async function excelFunction() {
+  //         const data = await workbook.xlsx.writeFile(v4() + ".xlsx");
 
-        const buffer = await workbook.xlsx.writeBuffer(
-          "excelSheet of student " + fromDate + "-" + lastDate + ".xlsx"
-        );
+  //         const buffer = await workbook.xlsx.writeBuffer(
+  //           "excelSheet of student " + fromDate + "-" + lastDate + ".xlsx"
+  //         );
 
-        // console.log('bbbbbbbbb', buffer);
+  //         // console.log('bbbbbbbbb', buffer);
 
-        let mailTransporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: "jayakrishnan@scriptlanes.com",
-            pass: "sivzgeycbgqpmsnz",
-          },
-        });
+  //         let mailTransporter = nodemailer.createTransport({
+  //           service: "gmail",
+  //           auth: {
+  //             user: "jayakrishnan@scriptlanes.com",
+  //             pass: "sivzgeycbgqpmsnz",
+  //           },
+  //         });
 
-        let details = {
-          from: "jayakrishnan@scriptlanes.com",
-          to: "jayakrishnansfc43@gmail.com",
-          subject: "Student details in Excel file",
-          // text: "Testing out first sender"
-          html: "Students details",
-          attachments: [
-            {
-              buffer,
-              content: buffer,
-              contentType:
-                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            },
-          ],
-        };
+  //         let details = {
+  //           from: "jayakrishnan@scriptlanes.com",
+  //           to: "jayakrishnansfc43@gmail.com",
+  //           subject: "Student details in Excel file",
+  //           // text: "Testing out first sender"
+  //           html: "Students details",
+  //           attachments: [
+  //             {
+  //               buffer,
+  //               content: buffer,
+  //               contentType:
+  //                 "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  //             },
+  //           ],
+  //         };
 
-        await mailTransporter.sendMail(details, (err: any) => {
-          if (err) {
-            console.log("There is an error ...", err);
-          } else {
-            console.log("email has sent!");
-          }
-        });
+  //         await mailTransporter.sendMail(details, (err: any) => {
+  //           if (err) {
+  //             console.log("There is an error ...", err);
+  //           } else {
+  //             console.log("email has sent!");
+  //           }
+  //         });
 
-        res.send({
-          status: "success",
-          message: "file sent to mail successfully!",
-          // path: `${path}/users.xlsx`,
-        });
-      }
+  //         res.send({
+  //           status: "success",
+  //           message: "file sent to mail successfully!",
+  //           // path: `${path}/users.xlsx`,
+  //         });
+  //       }
 
-      excelFunction();
+  //       excelFunction();
 
-    } catch (error) {
-      res.send({
-        status: "error",
-        message: "Something went wrong",
-      });
-    }
-  });
+  //     } catch (error) {
+  //       res.send({
+  //         status: "error",
+  //         message: "Something went wrong",
+  //       });
+  //     }
+  //   });
 };
 
 // cron jobs fn................................
@@ -437,33 +442,22 @@ const exportUsers = async (req: Request, res: Response) => {
 //   );
 // });
 
-
-
 // Uploading files with multer    ................................
 const uploadFiles = async (req: Request, res: Response) => {
-
   try {
     // console.log('fffffffffff',req.file);
-    console.log('bbbbbbbbbbbb',req.files);
+    console.log("bbbbbbbbbbbb", req.files);
     // res.json({ message: "Successfully uploaded files" });
 
-    
     res.status(200).json({
       status: "success",
       message: "File created successfully!!",
     });
-    
-
   } catch (error) {
     console.log(error);
-    res.json({error})
+    res.json({ error });
   }
-}
-
-
-
-
-
+};
 
 export {
   allUsers,
@@ -472,5 +466,5 @@ export {
   deletePerson,
   searchUsers,
   exportUsers,
-  uploadFiles
+  uploadFiles,
 };
